49f65eb72d69bb2f496a72443bb2906e17e79b0f
mhmd009900 <91033643+mhmd009900@users.noreply.github.com>
2025-05-17 17:34:42 +0300

    Update models.py

--- models.py
+++ models.py
@@ -4,17 +4,17 @@ from datetime import datetime, timedelta
 
 class AdminUser(Base):
    __tablename__ = "admin_users"
-  id = Column(Integer, primary_key=True, index=True)
-  email = Column(String, unique=True, index=True)
-  hashed_password = Column(String)
+   id = Column(Integer, primary_key=True, index=True)
+   email = Column(String, unique=True, index=True)
+   hashed_password = Column(String)
 
 class APIToken(Base):
    __tablename__ = "api_tokens"
-  id = Column(Integer, primary_key=True, index=True)
-  token = Column(String, unique=True, index=True)
-  client_email = Column(String, index=True)
-  permission_type = Column(String)
-  gmail_account = Column(String)
-  created_at = Column(DateTime, default=datetime.utcnow)
-  expires_at = Column(DateTime, default=lambda: datetime.utcnow() + timedelta(days=30))
-  used = Column(Boolean, default=False)
+   id = Column(Integer, primary_key=True, index=True)
+   token = Column(String, unique=True, index=True)
+   client_email = Column(String, index=True)
+   permission_type = Column(String)
+   gmail_account = Column(String)
+   created_at = Column(DateTime, default=datetime.utcnow)
+   expires_at = Column(DateTime, default=lambda: datetime.utcnow() + timedelta(days=30))
+   used = Column(Boolean, default=False)
