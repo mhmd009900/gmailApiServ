22c6e819d417df7de0b7407741148b442221591e
mhmd009900 <91033643+mhmd009900@users.noreply.github.com>
2025-05-17 18:01:21 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,22 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+
 
 DATABASE_URL = "postgresql://postgres:fLQsnIlPxaSPGeLlShqaVWnZVRVZCcUu@tramway.proxy.rlwy.net:55093/railway"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-def init_db():
-  from models import AdminUser, APIToken
-  Base.metadata.create_all(bind=engine)
-
-def get_db():
-  db = SessionLocal()
-  try:
-    yield db
-  finally:
-    db.close()
