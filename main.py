7c93a2f3e55b8d4766ef34f059e82d3625868dfb
mhmd009900 <91033643+mhmd009900@users.noreply.github.com>
2025-05-17 15:15:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 import admin, client
 from token_manager import schedule_token_cleanup
 from database import init_db
+import os
+import uvicorn
 
 app = FastAPI()
 
@@ -12,3 +14,7 @@ app.include_router(client.router, prefix="/api", tags=["Client"])
 def startup_event():
     init_db()
     schedule_token_cleanup()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # تستخدم بورت متغير البيئة الذي يوفره Railway
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
