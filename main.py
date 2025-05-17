c6b8eb08505952cf9b7a13ddb8ac8ebd5dca91d3
mhmd009900 <91033643+mhmd009900@users.noreply.github.com>
2025-05-17 17:16:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
-import admin
-import client
+import admin, client
 from token_manager import schedule_token_cleanup
 from database import init_db
 
@@ -11,6 +10,12 @@ app.include_router(client.router, prefix="/api", tags=["Client"])
 
 @app.on_event("startup")
 def startup_event():
-    init_db()
-    schedule_token_cleanup()
-    print("Server is running...")
+print("Server is running...")
+init_db()
+schedule_token_cleanup()
+
+if name == "main":
+import uvicorn, os
+port = int(os.environ.get("PORT", 8000))
+uvicorn.run("main:app", host="0.0.0.0", port=port)
+
