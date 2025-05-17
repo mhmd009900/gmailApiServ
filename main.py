e3cbf70f297350bb2b89432e1abdafcda61aebb9
mhmd009900 <91033643+mhmd009900@users.noreply.github.com>
2025-05-17 18:57:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ from accounts import router as accounts_router
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
-
+@app.get("/health")
+def health():
+    return {"status": "ok"}
 app.include_router(accounts_router, prefix="/api")
