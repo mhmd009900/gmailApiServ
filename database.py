02dce693287fe114e14aa82478875ce6d43437fb
mhmd009900 <91033643+mhmd009900@users.noreply.github.com>
2025-05-17 18:42:21 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
+# database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL is not set")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
